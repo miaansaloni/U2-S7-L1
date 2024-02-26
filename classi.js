d62ae1c7c5e9fff8f8082fb869afb3e60125dce5
Mia <155841134+miaansaloni@users.noreply.github.com>
2024-02-26 17:32:15 +0100

    second commit

--- classi.js
+++ classi.js
@@ -43,43 +43,43 @@ let addButton = document.getElementById("addPet");
 const formNode = document.querySelector("form");
 
 let pets = [];
-
-class Pet {
-  constructor(_petName, _ownerName, _species, _breed) {
-    this.ownerName = _ownerName;
-    this.petName = _petName;
-    this.species = _species;
-    this.breed = _breed;
-  }
-  isSameOwner(otherPet) {
-    if (this.ownerName === otherPet.ownerName) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
-
-const list = function () {
-  petList.innerHTML = "";
-  pets.forEach((pet) => {
-    const newLi = document.createElement("li");
-    newLi.innerText = "Name: " + pet.petName + ", owner: " + pet.ownerName;
-    petList.appendChild(newLi);
-  });
-};
-
-addButton.onclick = function () {
-  let newPet = new Pet(petName.value, ownerName.value, species.value, breed.value);
-  pets.push(newPet);
-  list();
-  petName.value = "";
-  ownerName.value = "";
-  species.value = "";
-  breed.value = "";
-};
-
 formNode.onsubmit = function (e) {
   e.preventDefault();
+
+  class Pet {
+    constructor(_petName, _ownerName, _species, _breed) {
+      this.ownerName = _ownerName;
+      this.petName = _petName;
+      this.species = _species;
+      this.breed = _breed;
+    }
+    isSameOwner(otherPet) {
+      if (this.ownerName === otherPet.ownerName) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  const list = function () {
+    petList.innerHTML = "";
+    pets.forEach((pet) => {
+      const newLi = document.createElement("li");
+      newLi.innerText = "Name: " + pet.petName + ", owner: " + pet.ownerName;
+      petList.appendChild(newLi);
+    });
+  };
+
+  addButton.onclick = function () {
+    let newPet = new Pet(petName.value, ownerName.value, species.value, breed.value);
+    pets.push(newPet);
+    list();
+    petName.value = "";
+    ownerName.value = "";
+    species.value = "";
+    breed.value = "";
+  };
+
   console.log("form inviato");
 };
